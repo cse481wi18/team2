fbf9b8fb4bc1de8262ad8c00dd91ce203b8abfe7
team2 <wangz59@uw.edu>
2018-01-18 11:57:21 -0800

    lab8 finished

--- fetch_api/src/fetch_api/arm.py
+++ fetch_api/src/fetch_api/arm.py
@@ -1,10 +1,14 @@
-# TODO: import ?????????
-# TODO: import ???????_msgs.msg
-# TODO: import ??????????_msgs.msg
+from trajectory_msgs.msg import JointTrajectory
+from trajectory_msgs.msg import JointTrajectoryPoint
+from control_msgs.msg import FollowJointTrajectoryAction
+from control_msgs.msg import FollowJointTrajectoryGoal
+
 import rospy
+import actionlib
 
 from .arm_joints import ArmJoints
 
+ACTION_NAME = "arm_controller/follow_joint_trajectory"
 
 class Arm(object):
     """Arm controls the robot's arm.
@@ -19,7 +23,8 @@ class Arm(object):
     def __init__(self):
         # TODO: Create actionlib client
         # TODO: Wait for server
-        pass
+        self.client = actionlib.SimpleActionClient(ACTION_NAME, FollowJointTrajectoryAction)
+        self.client.wait_for_server()
 
     def move_to_joints(self, arm_joints):
         """Moves the robot's arm to the given joints.
@@ -32,10 +37,20 @@ class Arm(object):
         # TODO: Set position of trajectory point
         # TODO: Set time of trajectory point
 
+        trajectory = JointTrajectory()
+        trajectory.joint_names = ArmJoints.names()
+        point = JointTrajectoryPoint()
+        point.positions = arm_joints.values()
+        point.velocities = [0.0] * len(ArmJoints.names())
+        trajectory.points = [point]
+
         # TODO: Create goal
         # TODO: Add joint name to list
         # TODO: Add the trajectory point created above to trajectory
+        goal = FollowJointTrajectoryGoal()
+        goal.trajectory = trajectory
 
         # TODO: Send goal
         # TODO: Wait for result
-        rospy.logerr('Not implemented.')
+        self.client.send_goal(goal)
+        self.client.wait_for_result(rospy.Duration.from_sec(5.0))
---
+++ joint_state_reader/src/joint_state_reader/reader.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python                                                                                  
+                                                                                                       
+import rospy  
+from sensor_msgs.msg import JointState      
+                                                                                                       
+class JointStateReader(object):  
+    def get_joint_msgs(self, msg):
+        self.joints_msg = msg
+
+    """Listens to /joint_states and provides the latest joint angles.                                  
+                                                                                                       
+    Usage:                                                                                             
+        joint_reader = JointStateReader()                                                              
+        rospy.sleep(0.1)                                                                               
+        joint_reader.get_joint('shoulder_pan_joint')                                                   
+        joint_reader.get_joints(['shoulder_pan_joint', 'shoulder_lift_joint'])                         
+    """                                                                                                
+    def __init__(self):                                                                                
+
+        self.sub = rospy.Subscriber('joint_states', JointState, self.get_joint_msgs)
+        self.joints_msg = None                                                                                     
+                                                                                                       
+    def get_joint(self, name):                                                                         
+        """Gets the latest joint value.                                                                
+                                                                                                       
+        Args:                                                                                          
+            name: string, the name of the joint whose value we want to read.                           
+                                                                                                       
+        Returns: the joint value, or None if we do not have a value yet.                               
+        """
+
+        for i in range(len(self.joints_msg.name)):
+            if self.joints_msg.name[i] == name:
+                return self.joints_msg.position[i]
+
+        return None                                                                                     
+                                                                                                       
+    def get_joints(self, names):                                                                       
+        """Gets the latest values for a list of joint names.                    
+                                                                                
+        Args:                                                                   
+            name: list of strings, the names of the joints whose values we want 
+                to read.                                                        
+                                                                                
+        Returns: A list of the joint values. Values may be None if we do not    
+            have a value for that joint yet.                                    
+        """
+
+        pos_list = []
+
+        for name in names:
+            pos_list.append(self.get_joint(name))            
+
+        return pos_list
